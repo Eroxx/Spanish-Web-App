3c4b808f4b597abd6f731671ea86a1e43d01c20e
Eroxx <Eric.Linder@gmail.com>
2026-04-25 14:29:21 -0400

    Add network-first service worker

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "spanish-sentences-v1";
+
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
